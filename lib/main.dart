2e28cc76ed8c3daa3d4145ae6d7d043619d46d6c
Tom McAloon <tmcal001@gold.ac.uk>
2023-03-16 15:14:26 +0000

    Updates to on Trello

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ Future<void> main() async {
 WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp().then((value) => Get.put(AuthController()));
 SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
